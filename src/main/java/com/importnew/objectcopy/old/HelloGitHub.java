ea06207a10d4d77c50959d2278587005d410fe7a
wangtuo <wangtuo1023@163.com>
2017-08-31 01:31:33 +0800

    新增测试

---
+++ src/main/java/com/importnew/objectcopy/old/HelloGitHub.java
@@ -0,0 +1,10 @@
+package com.importnew.objectcopy.old;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/importnew/objectcopy/test/HelloTest.java
@@ -0,0 +1,10 @@
+package com.importnew.objectcopy.test;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
